6440cfda08af329c869b1cd084af33f58aef21ba
Bryan-CoderS <139633909+Bryan-CoderS@users.noreply.github.com>
2023-10-02 23:33:49 +0800

    Attacking system
    +attacking system
    -a bug that makes it can't run

--- Creature/Dummy.gd
+++ Creature/Dummy.gd
@@ -1,5 +1,30 @@
 extends RigidBody2D
 
+var health = 10
+var p1_attackzone = false
+
+
 func _physics_process(_delta):
+	damaged()
+	
 	if Input.is_action_just_pressed("reset"):
 		global_position = Vector2(0, -50)
+
+
+
+func _on_hitbox_body_entered(body):
+	if body.has_method("player_1"):
+		p1_attackzone = true
+
+
+func _on_hitbox_body_exited(body):
+	if body.has_method("player_1"):
+		p1_attackzone = false
+
+func damaged():
+	if p1_attackzone and global.p1_attacking:
+		health = health - global.damage
+		print("Stop vandalism! Barrel health:", health)
+		if health <= 0:
+			self.queue_free()
+			print("I said STOP!!! You have broken the barrel!")
---
+++ global.gd
@@ -0,0 +1,6 @@
+extends Node
+
+var p1_attacking = false
+var damage = 1
+
+
