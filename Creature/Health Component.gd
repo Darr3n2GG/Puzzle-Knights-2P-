689a4ed2dd3a1c4b72fa07b7aa7ed0b92783cd0f
Darr3n <darrensoonyuchien@gmail.com>
2023-10-13 21:52:19 +0800

    saved health and hitbox component as scene

--- Creature/Health Component.gd
+++ Creature/Health Component.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Node
 class_name Com_HP
 
 @export var MaxHealth = 10
@@ -8,7 +8,7 @@ var health : int
 func _ready():
 	health = MaxHealth
 
-func damage(attack:Attack): #Bruh I need to watch another video to know how to fix this, worst tutorial ever
+func damage(attack : Attack): #Bruh I need to watch another video to know how to fix this, worst tutorial ever
 	health -= attack.attack_damage
 	if health <= 0:
 		get_parent().queue_free()
