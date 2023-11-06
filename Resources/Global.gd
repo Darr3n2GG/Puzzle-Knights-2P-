b45525890642bd3af159458ddc509a6b43ef9085
Darr3n <darrensoonyuchien@gmail.com>
2023-11-06 19:19:58 +0800

    Fixed main menu issue
    -Fixed main menu bug where it wouldn't load bc of global autoload
    -Changed main run scene to main menu scene

--- Resources/Global.gd
+++ Resources/Global.gd
@@ -1,14 +1,15 @@
 extends Node
 
-@onready var splitscreen = get_node("/root/Node")
+@onready var splitscreen = get_node("/root/Node") if get_tree().current_scene.name != "Menu" else null
+@onready var root = get_tree().root
 var current_level_node = null
 var current_level = 1
 
 func _ready():
-	var root = get_tree().root
-	current_level_node = root.get_child(root.get_child_count() - 1).get_node("HBoxContainer/LeftViewportContainer/LeftSubViewport").get_child(1)
-	var current_level_scene = current_level_node.scene_file_path
-	current_level = current_level_scene.to_int()
+	if get_tree().current_scene.name != "Menu":
+		current_level_node = root.get_child(root.get_child_count() - 1).get_node("HBoxContainer/LeftViewportContainer/LeftSubViewport").get_child(1)
+		var current_level_scene = current_level_node.scene_file_path
+		current_level = current_level_scene.to_int()
 	
 	
 func change_scene(path):
@@ -19,7 +20,6 @@ func deferred_change_scene(path):
 	var next_scene = ResourceLoader.load(path)
 	current_level_node = next_scene.instantiate()
 	current_level_node.name = "Level " + str(current_level)
-	var root = get_tree().root
 	root.get_child(root.get_child_count() - 1).get_node("HBoxContainer/LeftViewportContainer/LeftSubViewport").add_child(current_level_node)
 	splitscreen._ready()
 
