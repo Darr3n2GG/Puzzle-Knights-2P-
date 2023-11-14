922749061f3260f4f848258955c324cd80bcde7d
Bryan-CoderS <139633909+Bryan-CoderS@users.noreply.github.com>
2023-11-14 11:55:53 +0800

    Golem?
    GOLEM

---
+++ Creature/Golem/Golem.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+var direction :int = 0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	move_and_slide()
+
+
+
+func _on_detect_box_area_entered(area):
+	if area == player:
+		print("Player detected")
