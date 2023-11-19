28eef153390f0361f91f51bf6d78f561144141a6
Bryan-CoderS <139633909+Bryan-CoderS@users.noreply.github.com>
2023-11-19 23:26:45 +0800

    Simple update
    Wait

--- Puzzle Components/Exit Door/Door_Sound.gd
+++ Puzzle Components/Exit Door/Door_Sound.gd
@@ -2,7 +2,7 @@ extends AudioStreamPlayer2D
 
 
 func _connect(door: bool):
-	var audio: AudioStream = preload("res://Imports/Castle Door Open - QuickSounds.com.mp3")
+	var audio: AudioStream = preload("res://Music/SFX/Castle Door Open - QuickSounds.com.mp3")
 	self.set_stream(audio)
 	if door ==  true:
 		play()
